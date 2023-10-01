c2648cc9436428c542db4e24bf6076cd0465af94
subhradwip01 <subhradwip01@gmail.com>
2023-10-01 17:26:38 +0530

    Setup, Login, Signup Done

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,55 @@
+
+import { cn } from "../libs/utils"
+import { buttonVariants } from "../UI/Button"
+import { UserAuthForm } from "../Component/UserAuthForm"
+import { Link } from "react-router-dom"
+import { Banner } from "../Component/Banner"
+
+export default function Login() {
+  return (
+    <>
+      <div className="container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          to="/signup"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Sign Up
+        </Link>
+        <Banner/>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your Information and Get Access to Smart Buddy
+              </p>
+            </div>
+            <UserAuthForm isSignUp={false}/>
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/Pages/TalkToExperts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TalkToExperts = () => {
+  return (
+    <div>TalkToExperts</div>
+  )
+}
+
+export default TalkToExperts
